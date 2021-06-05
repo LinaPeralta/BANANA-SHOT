53775e5356c89cdecb5c99c257dd1e8e6b5eac08
Natalia Betancourt <nataliabetancourtm@gmail.com>
2021-06-05 18:23:37 -0500

    Interactions added through methods

--- src/model/Interaction.java
+++ src/model/Interaction.java
@@ -9,5 +9,72 @@ public class Interaction {
 	public Interaction(PApplet app) {
 		this.app = app;
 	}
+	
+	public void draw() {
+		
+
+	}
+	
+	public void coinMonkey() {
+		
+
+
+	}
+	
+	public void monkeyBanana() {
+		
+		
+	}
+	
+	public void bananaBullet() {
+		
+		
+
+	}
+	
+	public void monkeyBullet() {
+		
+
+	}
+	
+	public void gorillaBullet() {
+		
+
+	}
+	
+	public void platforms() {
+		
+		
+	}
+	
+	public void initCoins() {
+		
+
+	}
+	
+	public void removeCoins() {
+		
+
+	}
+	
+	public void initCoinsGrabbed() {
+		
+
+	}
+	
+	public void initBananas() {
+		
+
+	}
+	
+	public void initPlatforms() {
+		
+
+	}
+	
+	public void monkeyMove() {
+		
+
+	}
 
 }
--- src/model/Monkey.java
+++ src/model/Monkey.java
@@ -1,13 +1,20 @@
 package model;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Monkey {
 
 	private PApplet app;
+	private PImage monkeyL, monkeyR;
 
 	public Monkey(PApplet app) {
 		this.app = app;
+		
+		//Images
+		monkeyL = app.loadImage("./data/images/monkeyL.png");
+		monkeyR = app.loadImage("./data/images/monkeyR.png");
+		
 	}
 
 }
