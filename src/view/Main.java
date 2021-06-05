4973015e731fc8a01ae3417e91a59c801ec2ffee
Natalia Betancourt <nataliabetancourtm@gmail.com>
2021-06-05 16:58:10 -0500

    Organized main

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,26 @@
 package view;
+import controlP5.ControlP5;
 import processing.core.PApplet;
 
 public class Main extends PApplet{
 
-
+	//Libraries
+	private ControlP5 cp5;
+	
+	//Start screen
+	
+	//Instruction screen
+	
+	//Play screen
+	
+	//Stat screen
+	
+	//Game over screen
+	
+	
+	//Variables
+	private int screen;
+	
 	
 	public static void main(String[] args) {
 		PApplet.main("view.Main");
@@ -18,21 +35,123 @@ public class Main extends PApplet{
 	@Override
 	public void setup() {
 		
-	
+		//Library
+		cp5 = new ControlP5(this);
+		//Start screen
+		
+		//Instruction screen
+		
+		//Play screen
+		
+		//Stat screen
+		
+		//Game over screen
+		
+		
+		//Variables
+		screen = 0;
+		
+		
 	}
 	
 	@Override
 	public void draw() {
+		background(255);
 		
-		
-		
-		
-		
-		
-		
+		switch (screen) {
+		//Start screen
+		case 0:
+			
+			break;
+		//Instructions screen
+		case 1:
+			
+			break;
+		//Play screen
+		case 2:
+			
+			break;
+		//Game over screen
+		case 3:
+	
+			break;
+		//Stat screen
+		case 4:
+	
+			break;
+		}	
 		
 	}
-
 	
+	@Override
+	public void mousePressed() {
+		
+		switch (screen) {
+		//Start screen
+		case 0:
+			
+			
+			
+			
+			
+			
+			
+			
+			break;
+		//Instructions screen
+		case 1:
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			break;
+		//Play screen
+		case 2:
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			break;
+		//Game over screen
+		case 3:
+	
+			
+			
+			
+			
+			
+			
+			
+			break;
+		//Stat screen
+		case 4:
+	
+			
+			
+			
+			
+			
+			
+			break;
+		}	
+
+	}
+
+	@Override
+	public void keyPressed() {
+
+	}
+
 	
 }
--- src/view/StartScreen.java
+++ src/view/StartScreen.java
@@ -1,14 +1,36 @@
 package view;
 
+import controlP5.ControlP5;
 import processing.core.PApplet;
+import processing.core.PFont;
+import processing.core.PImage;
 
 public class StartScreen {
 	
 	private PApplet app;
+	private ControlP5 cp5;
+	private PImage start;
+	private PFont font;
+	private boolean userBox;
+	private String noFill;
 
-	public StartScreen(PApplet app) {
+	public StartScreen(PApplet app, ControlP5 cp5) {
 		this.app = app;
+		this.cp5 = cp5;
 		
+		//Image and fonts
+		start = app.loadImage("./data/images/startscreen.png");
+		font = app.loadFont("./data/fonts/MontserratAlternates-Regular.otf");
+		
+		//Variables
+		userBox  = false;
+		noFill = "";
+		
+	}
+	
+	public void userTextBox() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
